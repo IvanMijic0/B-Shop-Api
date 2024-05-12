9e593adea73d6f9898c0b417a91c339fdafc292e
Ivan Mijic <ivanmijic159951@gmail.com>
2024-05-12 16:02:36 +0200

    config for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
+
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
@@ -10,3 +12,5 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Setup complete."
