9c154d10e921e3666db911e7a7a1d4b7443bd075
Ivan Mijic <ivanmijic159951@gmail.com>
2024-05-12 16:32:12 +0200

    config for deployment1

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 echo "Running composer"
+composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -10,7 +14,6 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-
 echo "Running seeders..."
 php artisan db:seed --force
 
